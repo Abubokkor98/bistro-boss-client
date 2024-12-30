0200fb6e4ebea7a47a69e6145fb8e894a54e2694
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-30 12:45:57 +0600

    Show Popular menu items in the home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category";
+import PopularMenu from "./PopularMenu/PopularMenu";
 
 export default function Home() {
   return (
     <div>
       <Banner></Banner>
       <Category></Category>
+      {/*ekhane banner type er ekta baki */}
+      <PopularMenu></PopularMenu>
     </div>
   );
 }
