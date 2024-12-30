bf2523499abd2767ffe9236538f84e0e72ee6848
Abubokkor98 <mail.abubokkor@gmail.com>
2024-12-30 10:29:01 +0600

    64-2 Setup routes, layout and footer

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from '../Pages/Shared/Footer'
+import Navbar from '../Pages/Shared/Navbar'
+
+export default function Main() {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+        <Footer></Footer>
+    </div>
+  )
+}
