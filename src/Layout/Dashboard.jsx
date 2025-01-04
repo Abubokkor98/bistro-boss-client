e004004f54e595644137c227bf940faf3d8bf5ca
Abubokkor98 <mail.abubokkor@gmail.com>
2025-01-04 11:26:38 +0600

    Cart data from custom hook and calculate total price

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { FaBook, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import {
+  FaCalendar,
+  FaHome,
+  FaList,
+  FaSearch,
+  FaShoppingBag,
+  FaShoppingCart,
+} from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
 import { MdReviews } from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
 
@@ -7,6 +15,10 @@ export default function Dashboard() {
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-400">
+        <div className="uppercase p-5">
+          <h2 className="text-3xl font-bold">bistro boss</h2>
+          <span className="text-xl font-semibold tracking-[0.5em]">RESTURANT</span>
+        </div>
         <ul className="menu">
           <li>
             <NavLink to={"/dashboard/user-home"}>
@@ -38,9 +50,34 @@ export default function Dashboard() {
               MY BOOKING
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to={"/"}>
+              <FaHome></FaHome>
+              HOME
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/menu"}>
+              <FaSearch></FaSearch>
+              MENU
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/order/salad"}>
+              <FaShoppingBag />
+              SHOP
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"/dashboard/cotact"}>
+              <FaMessage />
+              CONTACT
+            </NavLink>
+          </li>
         </ul>
       </div>
-      <div className="flex-1 bg-slate-400">
+      <div className="flex-1 bg-slate-400 p-6">
         <Outlet></Outlet>
       </div>
     </div>
--- src/Pages/Dashboard/cart/Cart.jsx
+++ src/Pages/Dashboard/cart/Cart.jsx
@@ -1,9 +1,68 @@
 import React from "react";
+import useCart from "../../../CustomHooks/useCart";
 
 export default function Cart() {
+  const [cart] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   return (
-    <div className="text-center">
-      <h2 className="text-4xl font-semibold">My Cart</h2>
+    <div>
+      <div className="flex justify-evenly">
+        <h2 className="text-4xl">My Orders: {cart.length}</h2>
+        <h2 className="text-4xl">Total Price: {totalPrice}</h2>
+        <button className="btn btn-primary">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>ITEM IMAGE</th>
+              <th>ITEM NAME</th>
+              <th>PRICE</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item) => (
+              <tr key={item._id}>
+                <th>
+                  <label>
+                    <input type="checkbox" className="checkbox" />
+                  </label>
+                </th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{item.name}</div>
+                      <div className="text-sm opacity-50">{item.category}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  Zemlak, Daniel and Leannon
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Purple</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
